682d50fc20eaf982bf3aef28f4df762a19cce7d8
NatiOesquer <nati.oesquer@hotmail.com>
2022-07-25 09:16:16 -0300

    creando componentes header,acerde,banner,experiencia

--- Porfolio/ProyectoFinal_NataliaOesquer/Proyecto_NataliaOesquer/src/app/app.module.ts
+++ Porfolio/ProyectoFinal_NataliaOesquer/Proyecto_NataliaOesquer/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoAPComponent } from './componentes/logo-ap/logo-ap.component';
+import { SocialComponent } from './componentes/social/social.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent,
+
+
   ],
   imports: [
     BrowserModule,
