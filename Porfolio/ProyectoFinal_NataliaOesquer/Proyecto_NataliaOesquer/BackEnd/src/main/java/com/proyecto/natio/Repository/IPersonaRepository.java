74d09fb635bb053db7b3c78a377d9ce5474ec2a2
NatiOesquer <nati.oesquer@hotmail.com>
2022-10-11 22:21:54 -0300

    back
    primero pasos en back

---
+++ Porfolio/ProyectoFinal_NataliaOesquer/Proyecto_NataliaOesquer/BackEnd/src/main/java/com/proyecto/natio/Repository/IPersonaRepository.java
@@ -0,0 +1,12 @@
+
+package com.proyecto.natio.Repository;
+
+import com.proyecto.natio.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long>{
+    
+}
---
+++ Porfolio/ProyectoFinal_NataliaOesquer/Proyecto_NataliaOesquer/BackEnd/src/test/java/com/proyecto/natio/NatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.natio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
